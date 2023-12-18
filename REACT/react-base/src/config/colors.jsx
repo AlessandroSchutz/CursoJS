9ed333d4ca04250f700f510fb6d14fa72ee354c8
Alessandro Schutz <alessandro231.schutz@hotmail.com>
2023-12-18 15:48:50 -0300

    react-base att
    react-base do curso, postado até actions e reducers

---
+++ REACT/react-base/src/config/colors.jsx
@@ -0,0 +1,7 @@
+export const primaryColor = '#C3073F';
+export const primaryDarkColor = '#1A1A1D';
+
+export const successColor = '#168821';
+export const infoColor = '#155BCB';
+export const errorColor = '#E52207';
+export const warningColor = '#F2AF29';
