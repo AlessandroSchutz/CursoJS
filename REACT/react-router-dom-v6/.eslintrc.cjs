e381003e471074a8349ae573563f0cb9d19748f0
Alessandro Schutz <alessandro231.schutz@hotmail.com>
2024-01-25 12:15:31 -0300

    react-router-dom

---
+++ REACT/react-router-dom-v6/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    ecmaVersion: 'latest',
+    requireConfigFile: false,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 2,
+    'import/prefer-default-export': 0,
+    'no-console': 0,
+    'default-param-last': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
---
+++ REACT/react-router-dom-v6/src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export const Post = () => {
+    return (
+        <div>
+        <h1>Post</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis vel exercitationem maxime ipsa porro fugit eligendi, quo nesciunt assumenda, voluptate molestiae nihil eum suscipit excepturi facere quaerat eaque earum voluptas?</p>
+        </div>
+    )
+}
