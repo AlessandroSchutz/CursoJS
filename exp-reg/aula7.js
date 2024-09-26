f0fb1264d10aa3b406fc64293018b047b726017b
Alessandro Schutz <alessandro231.schutz@hotmail.com>
2024-09-26 14:34:43 -0300

    fim do modúlo de expressões regulares

--- exp-reg/aula7.js
+++ exp-reg/aula7.js
@@ -6,7 +6,7 @@ const { cpfs2 } = require("./base");
 // m -> multiline
 
 // const cpf = "254.224.877-45";
-const cpfRegExp = /^(\d{3}\.){2}\d{3}\-\d{2}$/gm;
+const cpfRegExp = /(?:\d{3}\.){2}\d{3}-\d{2}/gm;
 // console.log(cpf.match(cpfRegExp));
 console.log(cpfs2);
 console.log(cpfs2.match(cpfRegExp));
